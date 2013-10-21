7eb372c4695c8efa81d4945e0bb722ea5f11242e
Anh Nguyen <khacanh@gmail.com>
2013-10-21 16:21:25 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiDoc::Application.config.secret_key_base = '0e52f1af1cd44d16922a94f4c35805f71fb216265ae359bd305cfbb9d5c57b943b086c1dce27d8ceca8b8b84b332558ad679b66a8ac71da247adf34188b68bd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiDoc::Application.config.session_store :cookie_store, key: '_api_doc_session'
